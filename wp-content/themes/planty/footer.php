4252eb3d1879322e79d32620946f258a471a00c7
Mikael Fourré <mikael6287@live.fr>
2022-11-19 09:10:35 +0100

    Création de la page nous rencontrer

--- wp-content/themes/planty/footer.php
+++ wp-content/themes/planty/footer.php
@@ -2,7 +2,13 @@
     <!-- /Main -->
 
     <!-- Footer -->
-    <footer id="footer" class="footer footer--drawn">
+    <footer id="footer" class="footer
+        <?php if (($id = get_the_ID()) === 13): ?>
+            footer--drawn
+        <?php elseif ($id === 87): ?>
+            footer--colored footer--drawn
+        <?php endif; ?>
+    ">
         <a href="#" class="footer__link">Mentions légales</a>
     </footer>
     <!-- /Footer -->
