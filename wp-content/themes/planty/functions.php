0e90be0ee0e1cd217e30ae6cc1820a4e134a10e2
Mikael Fourré <mikael6287@live.fr>
2022-11-19 14:06:03 +0100

    Simplifier la modification des images

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -19,7 +19,15 @@ function custom_logo_setup() {
 	add_theme_support('custom-logo', $defaults);
 }
 
+function custom_image_template($html, $id, $caption, $title, $align, $url, $size, $alt) {
+	$thumb = wp_get_attachment_image_src($id, $size);
+	$image = '<img src="' . $thumb[0] . '" alt="' . $alt . '" />';
+
+	return $image;
+}
+
 
 add_filter('get_custom_logo', 'custom_logo_class');
 add_filter('show_admin_bar', '__return_false');
+add_filter('image_send_to_editor', 'custom_image_template', 1, 8);
 add_action('after_setup_theme', 'custom_logo_setup');
